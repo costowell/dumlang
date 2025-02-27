feca272c648bb8dbd72cd8ab0119e33025a469f9
Cole Stowell <cole@stowell.pro>
2025-02-26 23:43:32 -0500

    fix: evaluate arith expressions correctly
    see commit 1041768
    
    only caveat here is that certain arith expressions fail
    because there aren't enough registers. However, this can be improved by
    traversing the longer branch first instead of always choosing the left
    branch. Then, the only expressions that fail would have to have
    *at least* 2^14 *leaf nodes* on the arith tree. In a previous
    commit (1041768) I expressed wanting to combine this register table
    concept with memory on the stack so that we could keep going for EVERY
    expression, but, honestly, I think I'd cover all the reasonable
    expressions with a more efficient evaluation order. This saves me all
    the problems of calculating non-variable stack values since my current
    system maps a variable name to an offset, and what's stopping you from
    just saying your variable is t0, ATwaTYWahw, or whatever my temp name
    is.
    
    Anyways, the plan is a more efficient algorithm, but I'm still really
    happy that what I have is generating working code for a considerable
    number of arith expressions.

--- src/codegen.c
+++ src/codegen.c
@@ -11,14 +11,25 @@
 #include <stdlib.h>
 #include <string.h>
 
-Elf64_Sym symtab[64];
+#define TEXT_SIZE 1024
+#define STRTAB_SIZE 256
+#define SYMTAB_SIZE 64
+
+Elf64_Sym symtab[SYMTAB_SIZE];
 size_t symtab_len;
 
-uint8_t text[1024];
+uint8_t text[TEXT_SIZE];
 size_t text_len;
 
-char strtab[256];
+char strtab[STRTAB_SIZE];
 unsigned int strtab_len;
+
+bool regtab[NUM_REGISTERS] = {
+  [RSP] = true,
+  [RBP] = true,
+  0
+};
+
 
 /* Utils */
 
@@ -38,9 +49,25 @@ void print_strtab() {
   printf("\n");
 }
 
+reg_t next_reg() {
+  for (int i = 0; i < NUM_REGISTERS; ++i) {
+    // this register is not in use
+    if (!regtab[i]) {
+      // now in use
+      regtab[i] = true;
+      return (reg_t)i;
+    }
+  }
+
+  errx(EXIT_FAILURE, "not enough registers available!");
+}
+
 void emit() {
   uint8_t *instr;
   uint8_t size = instr_flush(&instr);
+
+  if (text_len + size >= TEXT_SIZE)
+    errx(EXIT_FAILURE, "text section bigger than %d bytes", TEXT_SIZE);
 
   if (instr == NULL)
     errx(EXIT_FAILURE, "failed to write instruction");
@@ -156,34 +183,43 @@ void ret() {
 
 /* Write Machine Instructions */
 
-void evaluate_arith_expression(arith_expression_t *expr, reg_t result, scope_t *scope) {
-  switch (expr->type) {
-    case ARITH_NUM:
-      mov_imm64(result, expr->instance.int64);
-      break;
-    case ARITH_IDENT:
-      ;
-      const scope_var_t *scope_var = scope_get(scope, expr->instance.name);
-      if (scope_var == NULL)
-        errx(EXIT_FAILURE, "error: '%s' not found in scope", expr->instance.name);
-      mov_mem_offset_to_reg(RAX, RBP, -scope_var->position);
-      break;
-    case ARITH_OP:
-      ;
-      const arith_operation_t *op = expr->instance.op;
-      evaluate_arith_expression(op->lhs, RAX, scope);
-      evaluate_arith_expression(op->rhs, RCX, scope);
+reg_t _evaluate_arith_expression(arith_expression_t *expr, scope_t *scope) {
+  if (expr->type == ARITH_NUM) {
+    reg_t r = next_reg();
+    mov_imm64(r, expr->instance.int64);
+    return r;
+  } else if (expr->type == ARITH_IDENT) {
+    const scope_var_t *scope_var = scope_get(scope, expr->instance.name);
+    if (scope_var == NULL)
+      errx(EXIT_FAILURE, "error: '%s' not found in scope", expr->instance.name);
+    reg_t r = next_reg();
+    mov_mem_offset_to_reg(r, RBP, -scope_var->position);
+    return r;
+  } else if (expr->type == ARITH_OP) {
+    const arith_operation_t *op = expr->instance.op;
+    reg_t lhsr = _evaluate_arith_expression(op->lhs, scope);
+    reg_t rhsr = _evaluate_arith_expression(op->rhs, scope);
 
-      switch (op->op) {
-        case OP_ADD:
-          add_reg_to_reg(RAX, RCX);
-          break;
-        case OP_MUL:
-          imul_reg_to_reg(RAX, RCX);
-          break;
-      }
+    regtab[rhsr] = false;
+
+    switch (op->op) {
+    case OP_ADD:
+      add_reg_to_reg(lhsr, rhsr);
       break;
+    case OP_MUL:
+      imul_reg_to_reg(lhsr, rhsr);
+      break;
+    }
+    return lhsr;
+  } else {
+    errx(EXIT_FAILURE, "unknown expression type");
   }
+}
+
+void evaluate_arith_expression(arith_expression_t *expr, reg_t result,
+                               scope_t *scope) {
+  reg_t r = _evaluate_arith_expression(expr, scope);
+  mov_regs(result, r);
 }
 
 void evaluate_expression(expression_t *expr, reg_t result, scope_t *scope) {
@@ -191,12 +227,14 @@ void evaluate_expression(expression_t *expr, reg_t result, scope_t *scope) {
   evaluate_arith_expression(expr->instance.arith, result, scope);
 }
 
-void write_declare_statement(declare_statement_t *stmt, scope_t *scope, char **added_vars, uint8_t *added_vars_size) {
+void write_declare_statement(declare_statement_t *stmt, scope_t *scope,
+                             char **added_vars, uint8_t *added_vars_size) {
   // We can discard type for now since we know it has to be an INT
   if (scope_get(scope, stmt->name) != NULL)
     errx(EXIT_FAILURE, "error: '%s' already declared", stmt->name);
 
-  const scope_var_t *scope_var = scope_insert(scope, stmt->name, 8); // Size is by default 4 since INT is the only type
+  const scope_var_t *scope_var = scope_insert(
+      scope, stmt->name, 8); // Size is by default 4 since INT is the only type
   added_vars[(*added_vars_size)++] = stmt->name;
 
   evaluate_expression(stmt->expr, RAX, scope);
@@ -210,10 +248,12 @@ void write_ret_statement(ret_statement_t *stmt, scope_t *scope) {
   ret();
 }
 
-void write_statement(statement_t *stmt, scope_t *scope, char **added_vars, uint8_t *added_vars_size) {
+void write_statement(statement_t *stmt, scope_t *scope, char **added_vars,
+                     uint8_t *added_vars_size) {
   switch (stmt->type) {
   case STMT_DECLARE:
-    write_declare_statement(stmt->instance.declare, scope, added_vars, added_vars_size);
+    write_declare_statement(stmt->instance.declare, scope, added_vars,
+                            added_vars_size);
     break;
   case STMT_RET:
     write_ret_statement(stmt->instance.ret, scope);
@@ -222,7 +262,7 @@ void write_statement(statement_t *stmt, scope_t *scope, char **added_vars, uint8
 }
 
 void write_codeblock(code_block_t *block, scope_t *scope) {
-  char *added_vars[8] = { 0 };
+  char *added_vars[8] = {0};
   uint8_t added_vars_size = 0;
   for (statement_t **stmts = block->statements; *stmts != NULL; ++stmts) {
     statement_t *stmt = *stmts;
