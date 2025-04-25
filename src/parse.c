5fe8822bc95d82f89ea0fbf1b24ae5e4a89ebd4a
Cole Stowell <cole@stowell.pro>
2025-04-25 14:42:23 -0400

    fix: implement try_parse functions correctly
    Didn't take long to find that my trys weren't implemented correctly.
    
    The best solution I could come up with was to have a fail label that
    each ASSERT macro would jump to. The main reason is to reset the lexer
    position on fail, but its flexible enough to free and cleanup any data
    too.
    
    A cool realization I made after rethinking my approach was that one
    "stable position" variable is not enough when chaining a bunch of try
    calls together. I imagine little markers in the string that you can
    always jump back to. It was very reminiscent of the problem of recursive
    function calls in assembly, how you have to push the return address onto
    the stack if you're going to call a function. If you have a function
    call chain of size N, you need to save the return address N-1 times, in
    much the same way that if you need to parse N nested grammatical
    structures, you need N-1 'stable positions'.

--- src/parse.c
+++ src/parse.c
@@ -6,13 +6,12 @@
 #include <stdlib.h>
 
 char error_msg[128] = {0};
-long stable_pos = 0;
 
 #define ASSERT_TOKEN(TOKEN_TYPE)                                               \
   do {                                                                         \
     if (!try_parse_token(TOKEN_TYPE)) {                                        \
       gen_token_error(TOKEN_TYPE);                                             \
-      return NULL;                                                             \
+      goto fail;                                                               \
     }                                                                          \
   } while (0)
 
@@ -22,29 +21,12 @@ long stable_pos = 0;
         try_parse_token_value(TOKEN_TYPE);                                     \
     if (__assert_token_value_var == NULL) {                                    \
       gen_token_error(TOKEN_TYPE);                                             \
-      return NULL;                                                             \
+      goto fail;                                                               \
     }                                                                          \
     __assert_token_value_var;                                                  \
   })
 
-#define NULL_RESTORE(CODE...)                                                  \
-  ({                                                                           \
-    void *_null_restore = CODE;                                                \
-    if (_null_restore == NULL) {                                               \
-      lex_set_pos(stable_pos);                                                 \
-    }                                                                          \
-    _null_restore;                                                             \
-  })
-
 #define ERRX(STATUS) errx(STATUS, "error: %s\n", error_msg)
-
-#define TRANSACTION(CODE...)                                                   \
-  do {                                                                         \
-    lex_set_pos(stable_pos);                                                   \
-    do                                                                         \
-      CODE while (0);                                                          \
-    stable_pos = lex_get_pos();                                                \
-  } while (0)
 
 void gen_token_error(token_type_t type) {
   snprintf(error_msg, sizeof(error_msg) - 1, "failed to parse token '%s'",
@@ -52,58 +34,75 @@ void gen_token_error(token_type_t type) {
 }
 
 type_t try_parse_type() {
+  type_t type;
+  long prevpos = lex_get_pos();
   if (try_parse_token(TOKEN_TYPE_INT)) {
-    return TYPE_INT64;
+    type = TYPE_INT64;
   } else {
+    lex_set_pos(prevpos);
     return TYPE_NONE;
   }
+  return type;
 }
 
 vartype_t *try_parse_vartype() {
-  token_value_t *name = ASSERT_TOKEN_VALUE(TOKEN_IDENTIFIER);
+  token_value_t *name;
+  type_t type;
+  long prevpos = lex_get_pos();
+
+  name = ASSERT_TOKEN_VALUE(TOKEN_IDENTIFIER);
   ASSERT_TOKEN(TOKEN_COLON);
-  type_t type = try_parse_type();
+  type = try_parse_type();
 
   if (type == TYPE_NONE) {
     gen_token_error(TOKEN_TYPE_INT);
-    return NULL;
+    goto fail;
   }
 
   vartype_t *vartype = malloc(sizeof(vartype_t));
   vartype->name = name->str;
   vartype->type = type;
   return vartype;
+
+fail:
+  lex_set_pos(prevpos);
+  return NULL;
 }
 
 function_t *try_parse_func() {
   token_value_t *name;
   vartype_t **args = calloc(MAX_FUNC_ARGS, sizeof(vartype_t *));
   unsigned int argc = 0;
-  TRANSACTION({
-    ASSERT_TOKEN(TOKEN_AT);
-    name = ASSERT_TOKEN_VALUE(TOKEN_IDENTIFIER);
-    ASSERT_TOKEN(TOKEN_PAREN_LEFT);
+  long prevpos = lex_get_pos();
 
-    vartype_t *vartype;
-    if (!try_parse_token(TOKEN_PAREN_RIGHT)) {
+  ASSERT_TOKEN(TOKEN_AT);
+  name = ASSERT_TOKEN_VALUE(TOKEN_IDENTIFIER);
+  ASSERT_TOKEN(TOKEN_PAREN_LEFT);
+
+  vartype_t *vartype;
+  if (!try_parse_token(TOKEN_PAREN_RIGHT)) {
+    vartype = try_parse_vartype();
+    if (vartype == NULL)
+      goto fail;
+    args[argc++] = vartype;
+    while (try_parse_token(TOKEN_COMMA)) {
       vartype = try_parse_vartype();
       if (vartype == NULL)
-        ERRX(EXIT_FAILURE);
+        goto fail;
       args[argc++] = vartype;
-      while (try_parse_token(TOKEN_COMMA)) {
-        vartype = try_parse_vartype();
-        if (vartype == NULL)
-          ERRX(EXIT_FAILURE);
-        args[argc++] = vartype;
-      }
-      ASSERT_TOKEN(TOKEN_PAREN_RIGHT);
     }
-  });
+    ASSERT_TOKEN(TOKEN_PAREN_RIGHT);
+  }
 
   function_t *func = malloc(sizeof(function_t));
   func->name = name->str;
   func->args = args;
   return func;
+
+fail:
+  free(args);
+  lex_set_pos(prevpos);
+  return NULL;
 }
 
 function_t **try_parse_ast() {
